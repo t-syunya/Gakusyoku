cc444011ce7317ea2e02f9e660de38bd9118c132
t.syunya <t.syunyam@gmail.com>
2021-05-12 19:40:00 +0900

    更新

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from sqlalchemy import Integer, Column, String, ForeignKey, Table
+from sqlalchemy import Integer, Column, DateTime, String, ForeignKey, Table
 from sqlalchemy.orm import relation, scoped_session
 from sqlalchemy.orm.session import sessionmaker
 import sqlalchemy.ext.declarative
@@ -15,20 +15,23 @@ SessionMaker = sessionmaker(bind=engine)
 session = scoped_session(SessionMaker)
 
 
-class Admin(Base):
+class Admin(Base):  # 管理者
     __tablename__ = 'administrators'
-    id = Column(Integer, primary_key=True)
     user_id = Column(String)
     password = Column(String)
 
 
-
-
-class Menu(Base):
+class Menu(Base):  # メニュー
     __tablename__ = 'menus'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    genre = Column(String)
-    value = Column(Integer)
-    
+    name = Column(String)  # メニュー名
+    date = Column(DateTime)  # 日付
+    value = Column(Integer)  # 値段
+    genre = Column(String)  # ジャンル
+    flag = Column(Integer)  # 売り切れフラグ, 1：売り切れ
 
+    def __init__(self, name, date, value, genre):
+        self.name = name
+        self.date = date
+        self.value = value
+        self.genre = genre
+        self.flag = 0
