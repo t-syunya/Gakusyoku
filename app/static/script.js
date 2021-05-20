1fcaba2efc821f3940391fdb99964fd76fb44dd2
t.syunya <t.syunyam@gmail.com>
2021-05-20 11:03:55 +0900

    更新

--- app/static/script.js
+++ app/static/script.js
@@ -1,42 +1,81 @@
-/*
 Vue.component("special-menus", {
     template: `
-<div class="row">
-    <div v-for="menu of menus">{{menu}}
-    aaa
-    </div>
-</div>
+        <div>
+            <div v-if="set_a.data.name!==null">
+                <div class="card mb-3" style="max-width: 540px">
+                    <div class="row no-gutters">
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h4>Aセット</h4>
+                                <h5 class="card-title">{{set_a.name}}</h5>
+                                <p class="card-text">{{set_a.value}}円</p>
+                            </div>        
+                        </div>
+                        <div class="col-md-6 my-auto">
+                            <img class="card-img" src="/static/images/curry.jpeg">
+                            <a class="btn btn-primary" href="#">Details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px">
+                <div class="row no-gutters">
+                    <div class="col-md-6">
+                        <div class="card-body">
+                            <h4>Bセット</h4>
+                            <h5 class="card-title">{{set_b.name}}</h5>
+                            <p class="card-text">{{set_b.value}}円</p>
+                        </div>        
+                    </div>
+                    <div class="col-md-6 my-auto">
+                        <img class="card-img" src="/static/images/curry.jpeg">
+                        <a class="btn btn-primary" href="#">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 `,
     mounted: function () {
-        axios.get('/menu/search', this.data.genre).then(response => {
-            this.menus = response.data;
-            console.log(response.data);
+        axios.get('/menu/search', {params: {genre: "A"}}).then(response => {
+            this.set_a = response;
+        });
+        axios.get('/menu/search', {params: {genre: "B"}}).then(response => {
+            this.set_b = response;
         })
-        //this.menus = ["a", "b", "c"]
     },
     data: function () {
         return {
-            genre: "special",
-            menus: []
+            set_a: [],
+            set_b: []
         }
     },
     methods: {}
 });
-*/
 
 Vue.component("permanent-menus", {
     template: `
         <div>
             <div v-for="menu in menus.data">
-                <div class="row">   
-                    <p>{{menu.name}}</p>
+                <div class="card mb-3" style="max-width: 540px">
+                    <div class="row no-gutters">
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h5 class="card-title">{{menu.name}}</h5>
+                                <p class="card-text">{{menu.value}}円</p>
+                            </div>        
+                        </div>
+                        <div class="col-md-6 my-auto">
+                            <img class="card-img" src="/static/images/curry.jpeg">
+                            <a class="btn btn-primary" href="#">Details</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 `,
     mounted: function () {
         axios.get('/menu/search', {
-            params:{
+            params: {
                 genre: "permanent"
             }
         }).then(response => {
