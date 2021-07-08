cda61ea662c5adcf93c9941081ec13bc042af00e
t-syunya <tsyunyam@gmail.com>
2021-07-08 14:01:35 +0900

    ログイン処理変更

--- app/database.py
+++ app/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import relation, scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-filename = "gakusyoku.db"
-url = "sqlite:///" + filename
+# filename = "gakusyoku.db"
+# url = "sqlite:///" + filename
 
-# url = "postgresql+psycopg2://team7:password@localhost:8087/team7db"
+url = "postgresql+psycopg2://team7:nanahann@localhost/team7db"
 
 Base = declarative_base()
-engine = create_engine(url, echo=False, connect_args={"check_same_thread": False})
+engine = create_engine(url, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
